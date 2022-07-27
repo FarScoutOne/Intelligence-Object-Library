8cde9917cc8a95a81409d445b148d52b7a7dbb89
farscout <ryneps@gmail.com>
2022-07-27 15:03:13 -0700

    Replace owner text strings in tests with variable name and created user.json file

--- testPIR.py
+++ testPIR.py
@@ -3,18 +3,18 @@ from pir import PIR
 
 class TestIntelligence(unittest.TestCase):
     def setUp(self):
-        self.pir = PIR("Ryne Smith")
+        self.pir = PIR(ryne_smith)
         self.pir.enemy = "Harkonnen"
         self.pir.target = "Base"
         self.req_string = "Where will the Harkonnens establish a military base?"
 
     def test_add_owner(self):
-        self.pir.add_owner({"Samantha Smith", "Paul Atreides"})
-        self.assertIn("Samantha Smith", self.pir.owners)
+        self.pir.add_owner({samantha_smith, })
+        self.assertIn(samantha_smith, self.pir.owners)
 
     def test_remove_owner(self):
-        self.pir.remove_owner("Samantha Smith")
-        self.assertNotIn("Samantha Smith", self.pir.owners)
+        self.pir.remove_owner(samantha_smith)
+        self.assertNotIn(samantha_smith, self.pir.owners)
 
 if __name__ == '__main__':
     unittest.main()
